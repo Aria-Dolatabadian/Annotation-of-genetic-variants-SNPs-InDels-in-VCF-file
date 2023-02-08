62ca96793c4e89173a17d3be9afa0cc47de4f09f
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-08 14:34:32 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,3 @@
+from bioinfokit.analys import marker
+marker.vcf_anot(file='SL3.0ch00_imputed.vcf', gff_file='itag30_chr0.gff3', anot_attr='Note')
+# annotated file will be saved in the same directory (SL3.0ch00_imputed_anot.txt)
